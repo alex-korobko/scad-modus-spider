235e4a0b725dfabc46b9d2f7df239e5842f65cc9
kor <kor@34dc3d99-ab81-5ee6-7b87-ae7f7f2f6c0a>
2003-06-12 14:57:17 +0000

    Added prototype of function read_messages  -   reading, decode and put to PostgreSQL messages. Changed argument type of Move_Data.

--- trunk/converter/src/gcc_ntox86/proto.h
+++ trunk/converter/src/gcc_ntox86/proto.h
@@ -4,6 +4,7 @@
 /* ../main.c */
 int quit_app ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int main_WndOpenFile ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
-int Move_Data ( int file_name );
+int Move_Data ( char *file_name );
 int switch_options ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int releazed_app ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
+int read_messages ( char *filename );
