4fb8ca0d787d973a69124a295dc00fd6f218ec01
kor <kor@34dc3d99-ab81-5ee6-7b87-ae7f7f2f6c0a>
2003-06-30 13:16:02 +0000

    Added to log::save user prmicions to file (S_IRUSR | S_IWUSR & so on).

--- spider27_old/src/log.cpp
+++ spider27_old/src/log.cpp
@@ -76,29 +76,29 @@ int Log::AddMessage(int msgID, int stationID, int escalatorID, time_t* saveTime,
 
 int Log::Save(const char* fileName)
 {
-	FILE	*logFile;
+	int		logFile;
 	dword	size;
 	char	signature[] = "LOGDAT";
 	LogRecord* curRecord;
 	
-	if ((logFile = fopen(fileName, "w")))
+	if ((logFile = open(fileName, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)) < 0)
 	{
 		SysMessage(ERROR_MSG, "Fail to open log file [%s]", strerror(errno));
 		return 0;
 	}
 		
-	fwrite(signature, strlen(signature),1,logFile);
+	write(logFile, signature, strlen(signature));
 	size = records.GetSize();
-	fwrite(&size, sizeof(size),1,logFile);
+	write(logFile, &size, sizeof(size));
 	records.first();
 	while((curRecord = records.next()) != NULL)
 	{
-		fwrite( &curRecord->msgID, sizeof(curRecord->msgID),1,logFile);
-		fwrite(&curRecord->stationID, sizeof(curRecord->stationID),1,logFile);
-		fwrite(&curRecord->escalatorID, sizeof(curRecord->escalatorID),1,logFile);
-		fwrite(&curRecord->saveTime, sizeof(curRecord->saveTime),1,logFile);			
+		write(logFile, &curRecord->msgID, sizeof(curRecord->msgID));
+		write(logFile, &curRecord->stationID, sizeof(curRecord->stationID));
+		write(logFile, &curRecord->escalatorID, sizeof(curRecord->escalatorID));
+		write(logFile, &curRecord->saveTime, sizeof(curRecord->saveTime));			
 	}
-	fclose(logFile);
+	close(logFile);
 	SysMessage(INFO_MSG, "Save current log");
 
 	return 1;
